1e3850c60b6c1fac78feb19a5ab9c8066acafe9e
MarcoB2 <begazomarcobegazo@gmail.com>
2023-10-28 19:44:52 -0500

    Primer Commit Begazo Marco

---
+++ src/main/java/pe/edu/cibertec/DAWII_Cl2_BegazoMarco/DawiiCl2BegazoMarcoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_Cl2_BegazoMarco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2BegazoMarcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2BegazoMarcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_Cl2_BegazoMarco/DawiiCl2BegazoMarcoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_Cl2_BegazoMarco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2BegazoMarcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
